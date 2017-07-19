aab3fb07ee7db0b3ceb12fb8673d5fdb4e537381
ee <chengang2022@gmail.com>
2017-07-19 23:48:33 +0800

    history list

--- js/history.js
+++ js/history.js
@@ -1,20 +1,68 @@
-var onOptions = function(){
-    var newURL = "options.html";
-    chrome.tabs.create({ url: newURL });
+var searchResults = [];
+
+var insertRow = function(index, url, title, time){
+    var tbody = $( "#history_list").find('tbody');
+    var num = index;
+    var html = "<tr><th scope='row'>" + num + "</th>";
+    html += "<td>" + url + "</td>";
+    html += "<td>" + title + "</td>";
+    html += "<td>" + time + "</td></tr>";
+    //html += "<td><a href='#' class='delete_link'>X</a></td></tr>";
+    tbody.append(html);
 }
 
-var onTrack = function(){
+var onUpdTbl = function(){
 
+    var tbody = $( "#history_list").find('tbody');
+    tbody.empty();
+
+    for( var i = 0; i < searchResults.length; ++i){
+        var url = searchResults[i].url;
+        var title = searchResults[i].title;
+        var time = searchResults[i].lastVisitTime;
+        insertRow(i, url, title, time);
+    }
 }
 
-var onHistory = function(){
+var dateStrToTime = function(str){
+    var decomposed = str.split("-");
+    var date = new Date(decomposed[0], decomposed[1]-1, decomposed[2]);
+    //alert(date);
+    return date.getTime();
+}
 
+var onSearch = function() {
+    var from = $("#datepicker1").val();
+    var to = $("#datepicker2").val();
+    //alert(from);
+    chrome.history.search({
+        'text':'',
+        'startTime':dateStrToTime(from),
+        'endTime':dateStrToTime(to),
+        'maxResults':100000
+    }, function(historyItems){
+        for(var i = 0; i < historyItems.length; ++i){
+            searchResults.push(historyItems[i]);
+        }
+        alert(JSON.stringify(searchResults));
+        onUpdTbl();
+    });
+}
+
+var initDate = function () {
+    var d = new Date();
+    d.setDate(d.getDate() - 8);
+    $("#datepicker1").val(d.toISOString().substr(0, 10));
+
+    d.setDate(d.getDate() + 7);
+    $("#datepicker2").val(d.toISOString().substr(0, 10));
+
+    $("#search").click(onSearch);
 }
 
 $(document).ready(function(){
-   $( "#options" ).click(onOptions);
 
-   $( "#track" ).on("click", onTrack);
+    initDate();
 
-   $( "#history" ).on("click", onOptions);
+    onUpdTbl();
 });
--- js/options.js
+++ js/options.js
@@ -1,3 +1,29 @@
+var insertRow = function(hostname){
+    var tbody = $( "#hostname_list").find('tbody');
+    var num = tbody.children().length + 1;
+    var html = "<tr><th scope='row'>" + num + "</th>";
+    html += "<td>" + hostname + "</td>";
+    html += "<td><a href='#' class='delete_link'>X</a></td></tr>";
+    tbody.append(html);
+}
+
+var onUpdTbl = function(){
+
+    var tbody = $( "#hostname_list").find('tbody');
+    tbody.empty();
+    chrome.storage.sync.get( "hostnames", function(items){
+        if(!items.hostnames)
+          return;
+
+        for( i = 0; i < items.hostnames.length; ++i){
+            insertRow(items.hostnames[i]);
+        }
+
+        $( ".delete_link" ).click(onDelete);
+    });
+
+}
+
 
 var onTrack = function(){
 
@@ -7,20 +33,48 @@ var onTrack = function(){
         alert("Error: No url input!");
         return;
     }
-    chrome.storage.sync.set({"test":url}, function(){
-        alert(chrome.storage.sync.get("test", function(cfg){
-          alert(cfg);
-        }));
+
+    chrome.storage.sync.get( "hostnames", function(items){
+        //alert(items.hostnames);
+        if(!items.hostnames)
+          items.hostnames = [url];
+        else
+          items.hostnames.unshift(url);
+        //alert(items.hostnames);
+        chrome.storage.sync.set(items, function(){
+            //insertRow(url);
+            onUpdTbl();
+        });
     });
 
+
 }
+
+
 
 var onHistory = function(){
 
 }
 
-var onDelete = function(){
+var onDelete = function(a){
+    var hostname = $(this).parent().prev().text();
+    if(hostname){
+        //alert(hostname);
+        chrome.storage.sync.get( "hostnames", function(items){
+            if(!items.hostnames)
+              return;
 
+            var index = items.hostnames.indexOf(hostname);
+            if(index > -1){
+                items.hostnames.splice(index, 1);
+                chrome.storage.sync.set(items, function(){
+                    //insertRow(url);
+                    onUpdTbl();
+                });
+            }
+
+        });
+    }
 }
 
 var onImport = function(){
@@ -32,9 +86,15 @@ var onExport = function(){
 }
 
 
+
+
 $(document).ready(function(){
 
-   $( "#track" ).click(onTrack);
+    $( "#track" ).click(onTrack);
 
-   $( "#history" ).on("click", onHistory);
+    $( "#history" ).on("click", onHistory);
+
+
+
+    onUpdTbl();
 });
